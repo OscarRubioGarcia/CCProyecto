f47bfb2491b302a03f810b4d74d8ea7edcbe7d0a
Oscar Rubio Garcia <takitow33@hotmail.com>
2019-11-07 20:14:11 +0100

    Incorporacion inicial de API REST de prueba y test de ella Avanza #25

--- Main.py
+++ Main.py
@@ -4,9 +4,6 @@ from flask import Flask
 from flask_restful import Resource, Api, abort, reqparse
 from model import Noticia
 
-# import connexion
-# from connexion.resolver import RestyResolver
-
 app = Flask("DashBoard")
 api = Api(app)
 parser = reqparse.RequestParser()
@@ -22,10 +19,5 @@ class Main(Resource):
 api.add_resource(Main,'/','/status')
 
 if __name__ == '__main__':
-    # Provide the app and the directory of the docs
-    #app = connexion.App(__name__, specification_dir='swagger/')
-    # app.add_api('dashboard-docs.yaml', resolver=RestyResolver('api'))
-    # os.environ is handy if you intend to launch on heroku
-    # app.run(port=int(os.environ.get('PORT', 2020)))
     port = int(os.environ.get("PORT", 80))
     app.run(host="0.0.0.0", port=port, debug=False)
