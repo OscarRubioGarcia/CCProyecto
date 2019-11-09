5b22a430ef91c9dda097a3f9d9f695110c9eff9f
Óscar Rubio García <orubiogarcia@correo.ugr.es>
2019-11-09 20:28:27 +0100

    Arreglo de indentacion en el ficher test_api.py~avanza #32

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,39 +1,38 @@
 import os
 import unittest
- 
+
 from Main import app
- 
+
+
 class BasicTests(unittest.TestCase):
- 
+
     ############################
     #### setup and teardown ####
     ############################
- 
+
     # executed prior to each test
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
         self.app = app.test_client()
- 
+
     # executed after each test
     def tearDown(self):
         pass
- 
- 
-	###############
-	#### tests ####
-	###############
- 
+
+    ###############
+    #### tests ####
+    ###############
+
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
-
-	def test_main_page_error(self):
+    def test_main_page_error(self):
         response = self.app.get('/error', follow_redirects=True)
         self.assertEqual(response.status_code, 404)
- 
- 
+
+
 if __name__ == "__main__":
     unittest.main()
